0b90b15acafc456c8cf8e81cc1720b065d6e5806
gaurav chauhan <gc0300273@gmail.com>
2023-12-13 21:51:01 +0530

    reset previous code,initialized project with next.js+ graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogModule } from './blog/blog.module';
+import { AppResolver } from './app/app.resolver';
 
 @Module({
-  imports: [AuthModule]
+  imports: [ GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: true,
+  }),TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }), BlogModule,],
+  providers: [AppResolver]
+
 })
 export class AppModule {}
